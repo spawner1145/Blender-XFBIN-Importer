b91eec319a5190bc70f9b654dd1c4da38490c983
spawner1145 <spawner6@outlook.com>
2026-09-21 21:26:09 +0800

    init

---
+++ __init__.py
@@ -0,0 +1,20 @@
+import importlib.util
+
+blender_loader = importlib.util.find_spec('bpy')
+
+bl_info = {
+    "name": "XFBIN File Importer/Exporter",
+    "author": "SutandoTsukai181, HydraBladeZ, Dei, TheLeonX",
+    "version": (1, 5, 0),
+    "blender": (4, 0, 0),
+    "location": "File > Import-Export",
+    "description": "Import/Export XFBIN files found in CC2's Storm Engine games.",
+    "warning": "",
+    "doc_url": "https://github.com/SutandoTsukai181/cc2_xfbin_blender/wiki",
+    "wiki_url": "https://github.com/SutandoTsukai181/cc2_xfbin_blender/wiki",
+    "tracker_url": "https://github.com/SutandoTsukai181/cc2_xfbin_blender/issues",
+    "category": "Import-Export",
+}
+
+if blender_loader:
+    from .blender.addon import *
